b2784b3d3e05c4645abdfe5f3a657fdf9205efed
spinualexandru <okeidev@gmail.com>
2021-04-23 18:55:50 +0300

    1 file modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,14 @@ export class HomeComponent {
   errorMessage: string;
   isPasta: boolean;
   initialLoad: boolean = true;
+  isCopyPasta(code: string) {
+    return (
+      code.indexOf("SafeMath") > -1 &&
+      code.includes(
+        "restores this intuition by reverting the transaction when an"
+      )
+    );
+  }
   getTokenData(address: string, errorToast: IgxToastComponent) {
     this.initialLoad = false;
     this.loading = true;
@@ -31,8 +39,9 @@ export class HomeComponent {
       } else {
         this.hasError = false;
         this.errorMessage = "";
-        this.isPasta = data.result[0].SourceCode.indexOf("SafeMath") > -1;
-        console.log("Is pasta?", this.isPasta)
+        const sourceCode = data.result[0].SourceCode;
+        this.isPasta = this.isCopyPasta(sourceCode);
+        console.log("Is pasta?", this.isPasta);
       }
       this.tokenData = data;
     });
